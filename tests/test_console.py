9db647e542488f11ee5c3bd967aef8bfba9bcf74
meriembenayad <meriem.benayad@gmail.com>
2023-11-20 16:48:09 +0100

    unittest Update 1

--- tests/test_console.py
+++ tests/test_console.py
@@ -33,6 +33,29 @@ class TestConsole(unittest.TestCase):
             output = f.getvalue().strip()
             self.assertEqual(output, '')
 
+    def test_do_create_command(self):
+        """ Test do_create command """
+        with patch('sys.stdout', new=StringIO()) as fc:
+            # Test with no arguments
+            self.cli.onecmd('create')
+            output = fc.getvalue().strip()
+            self.assertEqual(output, '** class name missing **')
+
+            # Test with invalid class
+            self.cli.onecmd('create MyClass')
+            output = fc.getvalue().strip()
+            self.assertEqual(output, "** class doesn't exist **")
+
+            # Test with valid class and parameters
+            self.cli.onecmd('create BaseModel name="John" age=23')
+            output = fc.getvalue().strip()
+            self.assertEqual(output)
+
+            # Test if instance is created and attribute are set
+            instance = self.cli.storage.all()['BaseModel.' + output]
+            self.assertEqual(instance.name, 'John')
+            self.assertEqual(instance.age, 23)
+
 
 if __name__ == '__main__':
     unittest.main()
