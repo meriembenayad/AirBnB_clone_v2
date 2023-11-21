b56dd37ddc6fc74d16329b854d525e3590072d5a
meriembenayad <meriem.benayad@gmail.com>
2023-11-21 18:44:00 +0100

    Task 8: DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ Define class City """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -9,6 +10,8 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship("Places", backref="cities", cascade="all, delete")
 
     def __init__(self, *args, **kwargs):
         """ Initialize City instances """
