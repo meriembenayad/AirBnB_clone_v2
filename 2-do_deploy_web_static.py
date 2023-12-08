fe9d933f4bebc6274f7960532ab06fb6aced38c1
meriembenayad <meriem.benayad@gmail.com>
2023-12-08 01:17:58 +0100

    Task 2: Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,32 @@
 from fabric.api import local, put, run, env
 from datetime import datetime
 from os import path, mkdir
-# Import do_pack from 1-pack_web_static file
-from pack_web_static import do_pack
 
 env.hosts = ['3.85.196.229', '34.207.221.84']
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    # Get the current date and time in the format YYYYMMDDHHMMSS
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Define the path of the archive file to be created
+    file_path = "versions/web_static_{}.tgz".format(date)
+
+    # Check if the versions directory exists, if not create it
+    if not path.exists("versions"):
+        mkdir("versions")
+
+    # Use local command to create .tgz archive of web_static directory
+    local("tar -cvzf {} web_static".format(file_path))
+
+    # Check if the archive was created successfully
+    if path.exists(file_path):
+        return file_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
