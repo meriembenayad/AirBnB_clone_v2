985f7495400866f569e2372854fc8079f2ba9422
meriembenayad <meriem.benayad@gmail.com>
2023-12-09 14:24:15 +0100

    Task 3: Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Creates and distributes an archive to your web servers
+"""
+do_pack = __import__('2-do_deploy_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['3.85.196.229', '34.207.221.84']
+
+
+def do_deploy():
+    """
+    Creates & Distributes an archive to the web servers.
+    """
+    archive_path = do_pack()
+
+    if archive_path is None:
+        return False
+
+    return do_deploy(archive_path)
