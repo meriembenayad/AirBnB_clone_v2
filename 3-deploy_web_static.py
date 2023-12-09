0af5599a0cc3439ecebe70f4ccfcf1aafb4e0fc0
meriembenayad <meriem.benayad@gmail.com>
2023-12-09 14:38:51 +0100

    Task 3: Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,12 +4,12 @@ Creates and distributes an archive to your web servers
 """
 from fabric.api import env
 do_pack = __import__('2-do_deploy_web_static').do_pack
-do_deploy_archive = __import__('2-do_deploy_web_static').do_deploy
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['3.85.196.229', '34.207.221.84']
 
 
-def do_deploy():
+def deploy():
     """
     Creates & Distributes an archive to the web servers.
     """
@@ -18,4 +18,4 @@ def do_deploy():
     if archive_path is None:
         return False
 
-    return do_deploy_archive(archive_path)
+    return do_deploy(archive_path)
