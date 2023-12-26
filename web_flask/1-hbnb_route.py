03806a8ef9ee2111dbe4d0bd46f8329f972b1cf5
meriembenayad <meriem.benayad@gmail.com>
2023-12-26 19:31:59 +0100

    Task 1: HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Hello Flask """
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
