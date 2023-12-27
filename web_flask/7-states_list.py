7f41920da86ff154792c4943cef47a51bb218d3b
meriembenayad <meriem.benayad@gmail.com>
2023-12-27 04:00:09 +0100

    Task 8: List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  """
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def displayStates():
+    """ Display states list """
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tearDown(exception=None):
+    """ close DB """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
