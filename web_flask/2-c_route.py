64ca365f2ab5d8cde39cecd08ce0b13a7baf22ed
meriembenayad <meriem.benayad@gmail.com>
2023-12-27 01:03:56 +0100

    Task 2: C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,16 +6,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Hello HBNB! """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ HBNB """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text=None):
+def cText(text):
+    """ C <text> """
     return "C {}".format(text.replace("_", " "))
 
 
